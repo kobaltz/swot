645696c0a7742ab6c631a84a33d402cb21a6bf0a
Dave Kimura <dave@k-innovations.net>
2023-06-22 21:06:42 -0400

    Updated academic list - 20230622

--- release.sh
+++ release.sh
@@ -6,5 +6,9 @@ current_date=$(date +%Y%m%d)
 file_path="swot.gemspec"
 sed -i '' -E "s/(s\.version = \"[0-9\.]+\.)[0-9]{8}/\1$current_date/g" $file_path
 
+version_number=$(grep -o -E "s\.version = \"[0-9\.]+$current_date\"" $file_path | grep -o -E "[0-9\.]+$current_date")
+
 git commit -am "Updated academic list - $current_date"
 gem build swot.gemspec
+
+gem push swot-ruby-$version_number.gem
