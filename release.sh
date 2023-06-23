b928078a13e1bf40db58e4429fd84659a39c4a63
Dave Kimura <dave@k-innovations.net>
2023-06-22 21:07:40 -0400

    added git push to release script

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ sed -i '' -E "s/(s\.version = \"[0-9\.]+\.)[0-9]{8}/\1$current_date/g" $file_pat
 version_number=$(grep -o -E "s\.version = \"[0-9\.]+$current_date\"" $file_path | grep -o -E "[0-9\.]+$current_date")
 
 git commit -am "Updated academic list - $current_date"
+git push
 gem build swot.gemspec
 
 gem push swot-ruby-$version_number.gem
